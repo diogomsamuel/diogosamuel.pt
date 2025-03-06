21d2cc24122764eb081be2b14245a5b74c840fae
diogomsamuel <diogosamuelfs@gmail.com>
2025-03-06 10:37:01 +0000

    button text fixed

--- src/hooks/useMessages.js
+++ src/hooks/useMessages.js
@@ -4,7 +4,7 @@ export default function useMessages() {
   const [messages, setMessages] = useState({});
 
   useEffect(() => {
-    fetch("/locales/pt/messages.json")
+    fetch("/locales/pt-PT/messages.json")
       .then((res) => res.json())
       .then((data) => setMessages(data))
       .catch(() => console.error("Erro ao carregar mensagens."));
